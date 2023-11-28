eaea6efdc4ab2863609c9f894418aac044a7112e
Britta Rodenbeck <cheers@brittarodenbeck.com>
2023-11-28 10:01:38 -0600

    :sparkles: adding header with links to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,15 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <p>Home page</p>
+    <>
+      <header>
+        <nav>
+          <Link href="/performance">Performance</Link>
+          <Link href="/reliability">Reliability</Link>
+          <Link href="/scale">Scale</Link>
+        </nav>
+      </header>
+      <p>Home page</p>
+    </>
   );
 }
