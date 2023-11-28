6356d45b6ab06c3b8c227297774bf4e7efa5f0d1
Britta Rodenbeck <cheers@brittarodenbeck.com>
2023-11-28 10:04:20 -0600

    :recycle: abstracting header nav to layout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import Link from 'next/link';
-
 export default function Home() {
   return (
-    <>
-      <header>
-        <nav>
-          <Link href="/performance">Performance</Link>
-          <Link href="/reliability">Reliability</Link>
-          <Link href="/scale">Scale</Link>
-        </nav>
-      </header>
-      <p>Home page</p>
-    </>
+    <p>Home page</p>
   );
 }
